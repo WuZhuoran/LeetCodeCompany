4e98f6f3c1fed6769132009965cba51408ead5b0
WuZhuoran <munoliver007@gmail.com>
2018-01-11 17:39:00 -0500

    add problem 283

---
+++ Facebook/283_Move_Zeroes/Solution1/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length <= 1) {
+            return;
+        }
+
+        int j = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
