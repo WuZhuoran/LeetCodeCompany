0bc084a468625bec30e8c632cc62ac8b57193f14
WuZhuoran <munoliver007@gmail.com>
2018-01-18 13:32:49 -0500

    feat: add problem 277

---
+++ Facebook/277_Find_the_Celebrity/Solution1/Solution.java
@@ -0,0 +1,27 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        if(n == 0) {
+            return 0;
+        }
+
+        int res = 0;
+
+        for(int i = 1; i < n; i++) {
+            if(knows(res, i)) {
+                res = i;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(i != res && knows(res, i) || !knows(i, res)) {
+                return -1;
+            }
+        }
+
+
+        return res;
+    }
+}
